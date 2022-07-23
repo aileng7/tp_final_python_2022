c17cef41be888bcf80bfbd7f7847e0e629edaa8f
aileng7 <gonzalez_ailen@hotmail.com>
2022-07-22 22:34:01 -0300

    get

--- app.py
+++ app.py
@@ -6,15 +6,56 @@ from models import Operation
 app = Flask(__name__)
 
 
-@app.route('/operation', methods=['POST'])
-def inform_operation():
+@app.route('/operations', methods=['POST'])
+def inform_operations():
+    date = request.json['date']
     type_operation = request.json['type_operation']
     description = request.json['description']
     amount = request.json['amount']
-    new_operation = Operation(type_operation, description, amount)
+    new_operation = Operation(date, type_operation, description, amount)
     db.session.add(new_operation)
     db.session.commit()
-    return jsonify(new_operation)
+    return jsonify({'message': 'Operación registrada:'}, new_operation)
+
+
+@app.route('/operations', methods=['GET'])
+def consult_operations():
+    operations_list = Operation.query.all()
+    return jsonify(operations_list)
+
+
+@app.route('/operations/<id>', methods=['GET'])
+def consult_operations_id(id):
+    operation = Operation.query.get(id)
+    return jsonify(operation)
+
+
+@app.route('/operations/<date>', methods=['GET'])
+def consult_operations_date(date):
+    operation = Operation.query.get(date)
+    return jsonify(operation)
+
+
+@app.route('/operations/<type_operation>', methods=['GET'])
+def consult_operations_type(type_operation):
+    operation = Operation.query.get(type_operation)
+    return jsonify(operation)
+
+
+@app.route('/operations/<id>', methods=['PUT'])
+def update_task(id):
+    operation = Operation.query.get(id)
+    date = request.json['date']
+    type_operation = request.json['type_operation']
+    description = request.json['description']
+    amount = request.json['amount']
+    operation.date = date
+    operation.type = type_operation
+    operation.description = description
+    operation.amount = amount
+    db.session.add(operation)
+    db.session.commit()
+    return jsonify(operation)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -8,16 +8,17 @@ import db
 class Operation(db.Base):
     __tablename__ = "operation"
     id = Column(Integer, primary_key=True)
+    date = Column(String(8))
     type_operation = Column(String(7))
     description = Column(String(50))
     amount = Column(Float)
 
-    def __init__(self, type_operation: string, description: string, amount: float):
+    def __init__(self, date: string, type_operation: string, description: string, amount: float):
         super(Operation, self).__init__()
+        self.date = date
         self.type_operation = type_operation
         self.description = description
         self.amount = amount
 
     def __str__(self):
-        return f'Operation: {self.id}, {self.type_operation}, {self.description}, {self.amount}'
-
+        return f'Operation: {self.id}, {self.date}, {self.type_operation}, {self.description}, {self.amount}'
